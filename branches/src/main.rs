6a68aefb4cbfcaa3c33d188767569d533afb9346
GuilhermeDeconto <deconto100@gmail.com>
2021-01-11 23:40:18 -0300

    Branches -> if, loops

---
+++ branches/src/main.rs
@@ -0,0 +1,82 @@
+fn main() {
+    println!("Hello, world!");
+
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    divisible(6);
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {}", number);
+
+    rust_loops();
+}
+
+fn rust_loops() {
+    loop {
+        println!("again!");
+        break;
+    }
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
+
+fn divisible(x: i32) {
+    if x % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if x % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if x % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+}
